a91bf5eec3ec82a20f1fb2b62be78e65d187dc98
denny.k <denny.k@kakaocorp.com>
2020-08-12 23:14:28 +0900

    [iOS-3] UINavigationController Attached

--- denny/Mission01/Mission01-Denny/Mission01-Denny/Main/MainViewController.swift
+++ denny/Mission01/Mission01-Denny/Mission01-Denny/Main/MainViewController.swift
@@ -19,32 +19,13 @@ public class MainViewController: UIViewController, MainViewProtocol {
         presenter.attachView(view: self)
         view.backgroundColor = .talkWhite000s
         
-        initializeNavigationBarLayout()
-        setNavigationBarContent(title: "커피 전문점")
-    }
-    
-    private func initializeNavigationBarLayout() {
-        let width = self.view.frame.width
-        navigationBar = UINavigationBar(frame: CGRect(x: 0, y: 0, width: width, height: 44))
-        
-        view.addSubview(navigationBar!)
-        navigationBar?.addSubview(navigationTitleLabel)
-        
-        navigationBar?.snp.makeConstraints { make in
-            make.top.equalTo(view.safeAreaLayoutGuide.snp.top)
-            make.left.equalTo(view.safeAreaLayoutGuide.snp.left)
-            make.right.equalTo(view.safeAreaLayoutGuide.snp.right)
-        }
-        
-        navigationTitleLabel.snp.makeConstraints { make in
-            make.center.equalToSuperview()
-        }
+        self.navigationController?.navigationBar.shadowImage = UIImage()
+        self.navigationController?.navigationBar.barTintColor = .talkWhite000s
+        self.setNavigationBarContent(title: "커피 전문점")
     }
     
     public func setNavigationBarContent(title: String) {
-        navigationBar?.barTintColor = .talkWhite000s
-        navigationTitleLabel.text = title
-        navigationTitleLabel.font = .font17PBold
+        self.navigationItem.title = title
     }
     
     public func updateOrderView(orderList: [Order]) {
