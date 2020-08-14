af12e680b98ec5fff7e932d91dbeb72fce5b0fd0
kimhyebeen <kimhbin@naver.com>
2020-08-14 17:13:53 +0900

    add TextView for showing process

--- kimhyebeen/Mission01/CafeSystem/app/src/main/java/com/khb/cafesystem/Barista.kt
+++ kimhyebeen/Mission01/CafeSystem/app/src/main/java/com/khb/cafesystem/Barista.kt
@@ -6,11 +6,16 @@ import com.khb.cafesystem.items.MenuItem
 object Barista {
     private var cash = 0;
 
-    fun makeCoffee(menuItem: MenuItem): Coffee {
+    fun makeCoffee(
+        menuItem: MenuItem,
+        addProcessText: (String) -> Unit
+    ): Coffee {
+        addProcessText("barista : ${menuItem.getMenuName()}을 만들어 손님에게 건냅니다.\n")
         return Coffee(menuItem)
     }
 
-    fun recieveMoney(money: Int) {
+    fun recieveMoney(money: Int, addProcessText: (String) -> Unit) {
+        addProcessText("barista : ${money}원을 받았습니다.\n")
         cash += money
     }
 }
