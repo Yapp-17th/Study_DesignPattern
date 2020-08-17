ade2620db994bb2022825d6a19c26a9c78eaaa2e
koserim <btobang1@naver.com>
2020-08-18 01:54:06 +0900

    [iOS-22] Project Setting

---
+++ ObservingCafe/ObservingCafe/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  ObservingCafe
+//
+//  Created by 바보세림이 on 2020/08/18.
+//  Copyright © 2020 serim. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
