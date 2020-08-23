cdeeae9f29f9b90a948e00957ee79976e1782cfe
koserim <btobang1@naver.com>
2020-08-24 03:36:59 +0900

    [iOS-22] Project Setting

---
+++ serim/Mission02/ObservingCafe/ObservingCafe/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ObservingCafe
+//
+//  Created by 바보세림이 on 2020/08/24.
+//  Copyright © 2020 serim. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
