e71ba4d8e0af5aa1b872e58df4a84754caa83a8f
dohan <adfilm2@naver.com>
2020-09-05 10:46:11 +0900

    파일 삭제

--- HyerinKim/Mission03/Mission03/ViewController.swift
+++ HyerinKim/Mission03/Mission03/ViewController.swift
@@ -2,24 +2,17 @@
 //  ViewController.swift
 //  Mission03
 //
-//  Created by dohan on 03/09/2020.
+//  Created by dohan on 05/09/2020.
 //  Copyright © 2020 Dohan. All rights reserved.
 //
 
-import Cocoa
+import UIKit
 
-class ViewController: NSViewController {
+class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
         // Do any additional setup after loading the view.
-    }
-
-    override var representedObject: Any? {
-        didSet {
-        // Update the view, if already loaded.
-        }
     }
 
 
