aa3bfff49caf517f569c1b7df05e7b44946a9bdb
della-padula <della.kimko@gmail.com>
2020-09-12 21:53:10 +0900

    [iOS-81] CocoaPods 설정

---
+++ denny/Mission04/Mission04-Denny/Pods/Target Support Files/Pods-Mission04-Denny-Mission04-DennyUITests/Pods-Mission04-Denny-Mission04-DennyUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Mission04_Denny_Mission04_DennyUITests : NSObject
+@end
+@implementation PodsDummy_Pods_Mission04_Denny_Mission04_DennyUITests
+@end
