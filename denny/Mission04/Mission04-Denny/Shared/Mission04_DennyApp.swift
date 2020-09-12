1fe0581157fbd45bd8c689ee6dda9f0965f5d5c4
della-padula <della.kimko@gmail.com>
2020-09-12 21:41:54 +0900

    [Apple-81] Project 생성, Swift UI for Multiplatform

---
+++ denny/Mission04/Mission04-Denny/Shared/Mission04_DennyApp.swift
@@ -0,0 +1,17 @@
+//
+//  Mission04_DennyApp.swift
+//  Shared
+//
+//  Created by denny on 2020/09/12.
+//
+
+import SwiftUI
+
+@main
+struct Mission04_DennyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
