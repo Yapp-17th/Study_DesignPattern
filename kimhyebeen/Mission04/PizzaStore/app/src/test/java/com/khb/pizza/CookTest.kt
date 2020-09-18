1d4f958ed31ece86ea8a497dbf15f0ac3d58bbe5
kimhyebeen <kimhbin@naver.com>
2020-09-18 16:37:48 +0900

    [Android-93] Cook의 unit test 작성

--- kimhyebeen/Mission04/PizzaStore/app/src/test/java/com/khb/pizza/CookTest.kt
+++ kimhyebeen/Mission04/PizzaStore/app/src/test/java/com/khb/pizza/CookTest.kt
@@ -1,16 +1,45 @@
 package com.khb.pizza
 
+import com.khb.pizza.items.OrderSheet
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.runBlocking
 import org.junit.jupiter.api.Test
 
 import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.DisplayName
+import org.mockito.Mock
+import org.mockito.Mockito
 
 internal class CookTest {
+    @Mock
+    val orderSheet = OrderSheet("Jamie", "potato", 500, "original", 'M', arrayListOf("pineapple"))
 
     @Test
+    @DisplayName("Cook가 일하고 있는지 제대로 확인되는지")
     fun getStatus() {
+        val exCook = Mockito.mock(Cook::class.java)
+        val beforeStatus = exCook.getStatus()
+        exCook.orderPizza(orderSheet)
+        val startStatus = exCook.getStatus()
+        runBlocking {
+            delay(1000)
+        }
+        val finishStatus = Cook.getStatus()
+
+        assertNotNull(Cook)
+        assertTrue(beforeStatus)
+        assertTrue(!startStatus)
+        assertTrue(beforeStatus == finishStatus)
     }
 
     @Test
+    @DisplayName("PizzaFactory에 요청하고 Pizza를 받아요")
     fun orderPizza() {
+        assertTrue(Cook.getStatus())
+
+        val pizza = Cook.orderPizza(orderSheet)
+
+        assertNotNull(pizza)
+        assertTrue(pizza.price == 11500)
     }
 }
