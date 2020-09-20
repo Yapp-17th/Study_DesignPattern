684e7970b53b34f2b8d79ba03c32551db1f5b465
dohan <adfilm2@naver.com>
2020-09-20 23:26:32 +0900

    [IOS-97]팩토리클래스, Store클래스 생성

---
+++ HyerinKim/Mission04/Mission4/PizzaFactory.swift
@@ -0,0 +1,13 @@
+//
+//  PizzaFactory.swift
+//  Mission4
+//
+//  Created by dohan on 20/09/2020.
+//  Copyright © 2020 Dohan. All rights reserved.
+//
+
+import Foundation
+
+protocol PizzaFactory{
+    
+}
---
+++ HyerinKim/Mission04/Mission4/PizzaStores.swift
@@ -0,0 +1,21 @@
+//
+//  PizzaStores.swift
+//  Mission4
+//
+//  Created by dohan on 20/09/2020.
+//  Copyright © 2020 Dohan. All rights reserved.
+//
+
+import Foundation
+
+class SoeulStore: PizzaFactory {
+    
+}
+
+class JejuStore: PizzaFactory {
+    
+}
+
+class BusanStore: PizzaFactory{
+    
+}
