7bf86fb5fde527d9838eabc978d542baf21e5f1e
kimhyebeen <kimhbin@naver.com>
2020-09-20 13:33:47 +0900

    [Android-95] 입력된 User 정보를 다음 화면으로 전달해주는 InputActivity 기능 구현

--- kimhyebeen/Mission04/PizzaStore/app/src/main/java/com/khb/pizza/InputActivity.kt
+++ kimhyebeen/Mission04/PizzaStore/app/src/main/java/com/khb/pizza/InputActivity.kt
@@ -1,11 +1,41 @@
 package com.khb.pizza
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.EditText
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_input.*
 
 class InputActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_input)
+
+        nextButton.setOnClickListener {
+            if (!isEmpty(nameEditText) && !isEmpty(numberEditText) && !isEmpty(addressEditText)) {
+                User(
+                    nameEditText.text.toString(),
+                    addressEditText.text.toString(),
+                    numberEditText.text.toString()
+                ).let {
+                    Intent(this, MainActivity::class.java).apply {
+                        putExtra("user", it)
+                        startActivity(this)
+                    }
+                }
+            } else {
+                toast("입력을 제대로 해주세요")
+            }
+        }
+    }
+
+    private fun isEmpty(target: EditText): Boolean {
+        if (target.text.toString() == "") return true
+        return false
+    }
+
+    private fun toast(str: String) {
+        Toast.makeText(this, str, Toast.LENGTH_SHORT).show()
     }
 }
--- kimhyebeen/Mission04/PizzaStore/app/src/main/java/com/khb/pizza/User.kt
+++ kimhyebeen/Mission04/PizzaStore/app/src/main/java/com/khb/pizza/User.kt
@@ -1,7 +1,9 @@
 package com.khb.pizza
 
+import java.io.Serializable
+
 data class User(
     val name: String,
     val address: String,
     val number: String
-)
+): Serializable
