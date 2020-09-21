572041799490f05c9ac053551ca9c47a3067a3c3
kimhyebeen <kimhbin@naver.com>
2020-09-21 16:36:08 +0900

    [Android-95] StatusActivity 뷰 수정

--- kimhyebeen/Mission04/PizzaStore/app/src/main/java/com/khb/pizza/StatusActivity.kt
+++ kimhyebeen/Mission04/PizzaStore/app/src/main/java/com/khb/pizza/StatusActivity.kt
@@ -2,10 +2,18 @@ package com.khb.pizza
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.khb.pizza.model.Menu
+import com.khb.pizza.model.User
 
 class StatusActivity : AppCompatActivity() {
+    private lateinit var user: User
+    private lateinit var menu: Menu
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_status)
+
+        user = intent.getSerializableExtra("user") as User
+        menu = intent.getSerializableExtra("menu") as Menu
     }
 }
