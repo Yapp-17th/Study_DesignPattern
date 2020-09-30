08d279586bbd4d887c8362a87673dde293dc8c4f
user <user@userui-MacBookPro.local>
2020-09-30 19:55:05 +0900

    Pizza Store Model 구현

--- denny/Mission04/Mission04-Denny/Mission04-Denny/PizzaStore.swift
+++ denny/Mission04/Mission04-Denny/Mission04-Denny/PizzaStore.swift
@@ -8,5 +8,32 @@
 import Foundation
 
 public class PizzaStore {
+    public static let shared: PizzaStore = PizzaStore()
     
+    private init() { }
+    
+    public func orderPizza(menu: String) -> PizzaModel {
+        var pizza: PizzaModel
+        pizza = createPizza(menu: menu)
+        
+        pizza.prepare()
+        pizza.bake()
+        pizza.cut()
+        pizza.box()
+        
+        return pizza
+    }
+    
+    public func createPizza(menu: String) -> PizzaModel {
+        if menu == "cheese" {
+            return CheezePizza()
+        } else if menu == "bulgogi" {
+            return BulgogiPizza()
+        } else if menu == "peperoni" {
+            return PeperoniPizza()
+        } else {
+            // Default Pizza
+            return CheezePizza()
+        }
+    }
 }
