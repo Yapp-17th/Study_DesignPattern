098743f66ffa1fd9623913351c084c776f2d406a
kimhyebeen <kimhbin@naver.com>
2020-10-21 18:48:16 +0900

    [Android-113] 버거의 재료를 선택하는 Add 화면 구현

--- kimhyebeen/Mission05/CustomBurger/app/src/main/java/com/khb/customburger/AddActivity.kt
+++ kimhyebeen/Mission05/CustomBurger/app/src/main/java/com/khb/customburger/AddActivity.kt
@@ -7,5 +7,6 @@ class AddActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_add)
+
     }
 }
