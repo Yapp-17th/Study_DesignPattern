ce42e56369575984d2f366fdcb47da7a210874c9
denny.k <denny.k@kakaocorp.com>
2020-10-25 11:05:45 +0900

    [iOS-127] Project Created, Project Basic Settings

---
+++ denny/Mission05/Mission05-Denny/Mission05-Denny/AppDelegate.swift
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.swift
+//  Mission05-Denny
+//
+//  Created by denny on 2020/10/25.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    var window: UIWindow?
+    
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+}
+
---
+++ denny/Mission05/Mission05-Denny/Mission05-DennyTests/Mission05_DennyTests.swift
@@ -0,0 +1,33 @@
+//
+//  Mission05_DennyTests.swift
+//  Mission05-DennyTests
+//
+//  Created by denny on 2020/10/25.
+//
+
+import XCTest
+@testable import Mission05_Denny
+
+class Mission05_DennyTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
