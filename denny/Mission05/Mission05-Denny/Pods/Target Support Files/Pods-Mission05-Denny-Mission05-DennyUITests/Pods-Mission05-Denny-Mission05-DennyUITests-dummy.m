68cbbd0db72f4003b3c5f446d996629c626c4ecf
denny.k <denny.k@kakaocorp.com>
2020-10-25 11:07:20 +0900

    [iOS-127] Cocoapods 기본 설정

---
+++ denny/Mission05/Mission05-Denny/Pods/Target Support Files/Pods-Mission05-Denny-Mission05-DennyUITests/Pods-Mission05-Denny-Mission05-DennyUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Mission05_Denny_Mission05_DennyUITests : NSObject
+@end
+@implementation PodsDummy_Pods_Mission05_Denny_Mission05_DennyUITests
+@end
---
+++ denny/Mission05/Mission05-Denny/Pods/Target Support Files/Pods-Mission05-DennyTests/Pods-Mission05-DennyTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Mission05_DennyTests : NSObject
+@end
+@implementation PodsDummy_Pods_Mission05_DennyTests
+@end
