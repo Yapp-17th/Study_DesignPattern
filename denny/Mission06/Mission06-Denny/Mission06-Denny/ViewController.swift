3e06bbd9208ee3d8d1623c8b1b5115170383b61f
denny.k <denny.k@kakaocorp.com>
2020-10-31 10:43:26 +0900

    Mission06 Project Initialization

---
+++ denny/Mission06/Mission06-Denny/Mission06-Denny/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Mission06-Denny
+//
+//  Created by denny on 2020/10/31.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
