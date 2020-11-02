d0be8340a3b296ff64172d2a628456d3f23a2084
kimhyebeen <kimhbin@naver.com>
2020-11-02 15:25:42 +0900

    [Android-140] 네트워크 작업을 수행하는 SearchRepository 생성 및 구현

--- kimhyebeen/Mission06/app/src/main/java/com/khb/dictionary/model/DictionaryItem.kt
+++ kimhyebeen/Mission06/app/src/main/java/com/khb/dictionary/model/DictionaryItem.kt
@@ -1,6 +1,10 @@
 package com.khb.dictionary.model
 
+import com.google.gson.annotations.SerializedName
+
 class DictionaryItem {
+    @SerializedName("title")
     var title: String? = null
+    @SerializedName("description")
     var description: String? = null
 }
---
+++ kimhyebeen/Mission06/app/src/main/java/com/khb/dictionary/model/SearchRepository.kt
@@ -0,0 +1,35 @@
+package com.khb.dictionary.model
+
+import android.util.Log
+import androidx.lifecycle.MutableLiveData
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+class SearchRepository {
+    var searchList = MutableLiveData<List<DictionaryItem>>()
+
+    private var retrofit: Retrofit = Retrofit.Builder()
+        .baseUrl("https://openapi.naver.com/v1/")
+        .addConverterFactory(GsonConverterFactory.create())
+        .build()
+    private var searchApi = retrofit.create(SearchApi::class.java)
+
+    fun requestDictionary(keyword: String) {
+        searchApi.getSearchList(keyword)
+            .enqueue(object : Callback<SearchItem> {
+                override fun onFailure(call: Call<SearchItem>, t: Throwable) {
+                    Log.e("SearchRepository", t.message ?: "error")
+                }
+                override fun onResponse(call: Call<SearchItem>, response: Response<SearchItem>) {
+                    response.body()?.items?.let { saveResult(it) }
+                }
+            })
+    }
+
+    fun saveResult(items: List<DictionaryItem>) {
+        searchList.value = items
+    }
+}
