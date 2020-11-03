c76695fb682452836bdd4d3dba15044ca45c2a3f
kimhyebeen <kimhbin@naver.com>
2020-11-03 15:54:12 +0900

    [Android-143] Adapter 생성 및 구현

--- kimhyebeen/Mission06/app/src/main/java/com/khb/dictionary/MainActivity.kt
+++ kimhyebeen/Mission06/app/src/main/java/com/khb/dictionary/MainActivity.kt
@@ -1,25 +1,51 @@
 package com.khb.dictionary
 
+import android.annotation.SuppressLint
+import android.app.Activity
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
+import android.view.View
+import android.view.inputmethod.InputMethodManager
 import androidx.databinding.DataBindingUtil
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.khb.dictionary.adapter.SearchAdapter
 import com.khb.dictionary.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
-    lateinit var binding: ActivityMainBinding
-    val vm = DictionaryViewModel()
+    private lateinit var binding: ActivityMainBinding
+    private val searchAdapter = SearchAdapter()
+    private val vm = DictionaryViewModel()
 
+    @SuppressLint("ClickableViewAccessibility")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
-        binding.lifecycleOwner = this
-        binding.setVariable(BR.vm, vm)
+        binding.apply {
+            lifecycleOwner = this@MainActivity
+            setVariable(BR.vm, vm)
+
+            activityView.setOnTouchListener { _, _ -> onHideKeypad() }
+            mainRecyclerView.apply {
+                adapter = searchAdapter
+                layoutManager = LinearLayoutManager(applicationContext)
+                setHasFixedSize(true)
+                setOnTouchListener { _, _ -> onHideKeypad() }
+            }
+        }
 
         vm.searchList.observe(this, {
-            it.map { println("${it.title} -> ${it.link}") }
+            searchAdapter.setContents(it)
         })
+    }
+
+    private fun onHideKeypad(): Boolean {
+        val imm = getSystemService(Activity.INPUT_METHOD_SERVICE) as InputMethodManager
+        val view: View = currentFocus ?: View(this)
+        view.clearFocus()
+        imm.hideSoftInputFromWindow(view.windowToken, 0)
+        return false
     }
 }
