5bb6cd67e015215ecc7d2d1ee0bab9518cec3404
kimhyebeen <kimhbin@naver.com>
2020-11-03 15:21:55 +0900

    [Android-143] 데이터바인딩 적용. Button을 누르면 EditText의 내용으로 검색 가능하도록 구현

--- kimhyebeen/Mission06/app/src/main/java/com/khb/dictionary/MainActivity.kt
+++ kimhyebeen/Mission06/app/src/main/java/com/khb/dictionary/MainActivity.kt
@@ -2,10 +2,24 @@ package com.khb.dictionary
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import androidx.databinding.DataBindingUtil
+import com.khb.dictionary.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
+    val vm = DictionaryViewModel()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        binding.lifecycleOwner = this
+        binding.setVariable(BR.vm, vm)
+
+        vm.searchList.observe(this, {
+            it.map { println("${it.title} -> ${it.link}") }
+        })
     }
 }
