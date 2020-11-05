a994cb986ecc7a75dc7a4163e5a6aae7d992768f
koserim <btobang1@naver.com>
2020-11-06 01:12:28 +0900

    [init] project setting

---
+++ serim/Mission06/MVVMPizza/MVVMPizza/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MVVMPizza
+//
+//  Created by 바보세림이 on 2020/11/06.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
