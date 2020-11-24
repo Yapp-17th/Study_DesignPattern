3792a70d007793c6dd2e2a8d17e0cec2a3d1c929
kimhyebeen <kimhbin@naver.com>
2020-11-24 15:11:46 +0900

    [Android-151] MainActivity 구현 (adapter 제외)

---
+++ kimhyebeen/Mission07/app/src/main/java/com/khb/dictionary/main/MainActivity.kt
@@ -0,0 +1,64 @@
+package com.khb.dictionary.main
+
+import android.annotation.SuppressLint
+import android.app.Activity
+import android.os.Bundle
+import android.view.View
+import android.view.inputmethod.InputMethodManager
+import android.widget.Toast
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.khb.dictionary.R
+import com.khb.dictionary.base.BaseActivity
+import com.khb.dictionary.base.MainContract
+import com.khb.dictionary.model.DictionaryItem
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity
+    : BaseActivity(R.layout.activity_main), MainContract.View {
+    private lateinit var presenter: MainPresenter
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setAdapter()
+        setButton()
+    }
+
+    override fun initPresenter() {
+        presenter = MainPresenter()
+        presenter.takeView(this)
+    }
+
+    @SuppressLint("ClickableViewAccessibility")
+    override fun setAdapter() {
+        main_recycler_view.apply {
+            // todo - adapter 설정
+            layoutManager = LinearLayoutManager(context)
+            setHasFixedSize(true)
+            setOnTouchListener { _, _ -> onHideKeypad() }
+        }
+    }
+
+    override fun setButton() {
+        main_search_button.setOnClickListener {
+            val keyword = main_search_edit_text.text.toString()
+            presenter.getDictionaryData(keyword)
+        }
+    }
+
+    override fun showDictionaryList(list: List<DictionaryItem>) {
+        // todo - adapter.setItems(list)
+    }
+
+    override fun showError(error: String) {
+        Toast.makeText(this, error, Toast.LENGTH_SHORT).show()
+    }
+
+    private fun onHideKeypad(): Boolean {
+        val imm = getSystemService(Activity.INPUT_METHOD_SERVICE) as InputMethodManager
+        val view: View = currentFocus ?: View(this)
+        view.clearFocus()
+        imm.hideSoftInputFromWindow(view.windowToken, 0)
+        return false
+    }
+}
