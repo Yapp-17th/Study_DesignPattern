cd6a5cfc89956664bda7c6a6317c0e905c682d40
kimhyebeen <kimhbin@naver.com>
2020-11-24 15:11:00 +0900

    [Android-151] MainPresenter 구현

---
+++ kimhyebeen/Mission07/app/src/main/java/com/khb/dictionary/main/MainPresenter.kt
@@ -0,0 +1,34 @@
+package com.khb.dictionary.main
+
+import android.util.Log
+import com.khb.dictionary.base.MainContract
+import com.khb.dictionary.model.NetworkModule
+import com.khb.dictionary.model.SearchItem
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+
+class MainPresenter: MainContract.Presenter {
+    private var view: MainContract.View? = null
+
+    override fun getDictionaryData(keyword: String) {
+        NetworkModule.searchApi.getSearchList(keyword)
+            .enqueue(object : Callback<SearchItem> {
+                override fun onFailure(call: Call<SearchItem>, t: Throwable) {
+                    Log.e("MainPresenter", t.message ?: "error")
+                    view?.showError("데이터 요청 실패")
+                }
+                override fun onResponse(call: Call<SearchItem>, response: Response<SearchItem>) {
+                    response.body()?.items?.let { view?.showDictionaryList(it) }
+                }
+            })
+    }
+
+    override fun takeView(view: MainContract.View) {
+        this.view = view
+    }
+
+    override fun dropView() {
+        this.view = null
+    }
+}
